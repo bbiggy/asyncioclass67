37a7b87b1e0bdc80cc5a6974e25ecd89717ce147
B_B <bbiggy.c2@gmail.com>
2024-06-28 11:47:23 +0700

    ass3_async2

--- assignment3/asyncio02.py
+++ assignment3/asyncio02.py
@@ -1,2 +1,14 @@
 # example of running a coroutine
+import asyncio
+async def custom_coro():
+    # await another coroutine
+    await asyncio.sleep(1)
+    
+# main corrutine
+async def main():
+    # execute my custom coroutine
+    await custom_coro()
+    
+# start the coroutine program
+asyncio.run(main())
 
